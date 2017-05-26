08b05c6e55136358cf06dad06d1e43a7820b3f1e
Camille Grace Bacister <bcamillegrace@gmail.com>
2017-05-26 10:25:10 +0800

    Update 153.py

--- 153.py
+++ 153.py
@@ -5,28 +5,30 @@ while 1:
 	px = raw_input("P(x): ")
 	choice = raw_input ("\n[1] A(x) + B(x)\n[2] A(x) - B(x)\n[3] A(x) x B(X)\n[4] A(x) / B(x) \n[5] Exit \nEnter choice of operation: ")
 
-
 	ax = [int(a) for a in ax.split(" ")]
 	bx = [int(b) for b in bx.split(" ")]
 	px = [int(p) for p in px.split(" ")]
 	cx = [0 for c in range (0, max(len(ax),len(bx)))]
 
+	
 	#defining length of a&b
 	lena = len(ax)
 	lenb = len(bx)
-
-	#defining exponent
-	if lena > lenb:	exponent = lena
-	elif lenb > lena: exponent = lenb
-
 	#input_validation
 	def error_printing(r, s):
+		#defining exponent
+		if lena > lenb:	exponent = lena
+		elif lenb > lena: exponent = lenb
+		elif lenb == lena: exponent = lena
+
 		for r in s:
 			if r < 0:	
 				print "\nERROR no negative coefficient."
+				exit(0)
 
 			elif r > 2**exponent-1:	
-				print "\nERROR Beyond maximum coefficient in "
+				print "\nERROR Beyond maximum coefficient."
+				exit(0)
 
 		
 			#elif r.isdigit() == False:
@@ -34,8 +36,16 @@ while 1:
 
 	error_printing(a, ax)
 	error_printing(b, bx)
-	error_printing(p, px)
+	for p in px:
+		if p < 0:	
+			print "\nERROR no negative coefficient."
+		elif (len(px) == 1):
+			print "ERROR invalid power input in P(x)."
+			exit(0)
 
+		elif (int(p) != 1 and int(p) != 0):
+			print "\nERROR Only binary input are accepted in P(x)."
+			exit(0)
 
 	#stuffing
 	i=0
@@ -51,11 +61,39 @@ while 1:
 			ax.insert(0,0)
 			i+=1
 
+	def mult(b, a, p_int, p_string):
+		b_binary = bin(b)[2:]
+		a_binary = bin(a)[2:]
+		b_int = int(b_binary)
+		a_int = int(a_binary)
+		product = b_int*a_int
+		prod_string = str(product)
+		modulo = ""
+		xor = ""
+		
+		for i in prod_string:
+			if int(i) > 1:
+				modulo += str(int(i)%2)
+			else:
+				modulo += i
+		product = int(modulo)
+		prod_string = modulo
+		if (len(prod_string) >= len(p_string)):
+			p_list = [0 for i in range(0,len(prod_string))]
+			prod_list = [0 for i in range(0,len(prod_string))]
+			if prod_string[0] == "0":
+				prod_string = prod_string[1:]
+				for j in range(0,len(prod_string)):
+					xor += str(int(prod_string[j])^int(p_string))
+		else:
+			xor = prod_string
+		print "Product: ", int(xor, 2)
+		return int(xor, 2)
+
 	#printing
 	print ("\n\nP R I N T I N G  V A L U E S")
 	print "A(x): ", ax
 	print "B(x): ", bx
-	print "choice", choice
 
 	def print_given(p):
 		power = len(p)-1
@@ -102,7 +140,7 @@ while 1:
 		print " ",
 		for m in ax:
 			print m, " ",
-		print "\n+",
+		print "\n-",
 		for m in bx:
 			print m, " ",
 		print "\n"
@@ -112,7 +150,35 @@ while 1:
 		print "\n\nA(x) - B(x) = ", print_given(cx)
 
 	#multiplication
-	#elif (choice == "3"):
+	elif (choice == "3"):
+		add = [0 for i in range(0, len(ax)+len(bx)-1)]
+		p_string = ""
+		#a_string = ''.join(str(a) for a in ax)
+		#b_string = ''.join(str(b) for b in bx)
+		#a_integer = ''.join(int(a) for a in a_string)
+		#b_integer = ''.join(int(b) for b in b_string)
+
+		for i in px:
+			p_string += str(i)
+		print p_string
+
+		p_int = int(p_string, 2)
+		print p_int
+
+		#print a_string
+		#print b_string
+
+		ctrb = -1
+		for i in bx:
+			ctrb += 1
+			ctra = ctrb - 1
+			for j in ax:
+				ctra += 1
+				print i
+				print j
+				add[ctra] = add[ctra]^mult(i, j, p_int, p_string)
+		print "C(x): ", add
+			
 
 	#division
 	#elif (choice == "4"):
